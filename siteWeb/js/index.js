a5e96dad06fedfe3310d2bec2eca35f99f7e1240
MatiasAma <Matias.amaglio6@etu.univ-lorraine.fr>
2024-06-11 10:59:30 +0200

    carte basique en dure Nancy

---
+++ siteWeb/js/index.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([48.688135, 6.171586 ], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    minZoom:12,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
